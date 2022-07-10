b9a06b8070acbb973791be3fc945e936c859aab0
Crunchybubbles <jasperspafford@gmail.com>
2022-07-09 21:03:36 -0700

    all pools all the ticks

--- scripts/u3sol.py
+++ scripts/u3sol.py
@@ -1,22 +1,97 @@
 import brownie
 from brownie import accounts, Uni3ticks
+from brownie.network import priority_fee
 import requests as r
 import math
 
+class Uni3Pool:
+    def __init__(self, query):
+        self.address = query['id']
+        self.token0 = query['token0']
+        # self.token0_symbol = token0_symbol
+        # self.token0_decimals = decimals0
+        # self.token1 = token1
+        # self.token1_symbol = token1_symbol
+        # self.token1_decimals = decimals1
+        # self.fee = fee
+        # self.createdAtblock = block
+        # self.createdAtTime = Time
+        # self.ticks = ticks
+        # self.current_tick = current_tick
+        
+        
+
+    
+
+
 MAX_U160 = ((2**160) - 1)
+
+def do_request(query):
+    req = r.post('https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3',json={'query':query})
+    if req.status_code == 200:
+        return req.json()
+    else:
+        raise Exception(f'Failed with return code {req.status_code, query}')
+    
+def getAllPools():
+    uni3pools = []
+    query1 = """
+    {pools(first:1,orderBy:createdAtTimestamp,orderDirection:asc) { 
+    id,
+    token0 {id,symbol,decimals},
+    token1{id,symbol,decimals},
+    feeTier,
+    createdAtBlockNumber,
+    createdAtTimestamp,
+    ticks(where:{liquidityNet_gt: 0}, orderBy:tickIdx, orderDirection:asc){
+        tickIdx,
+        liquidityNet,
+        liquidityGross,
+        price0,
+        price1
+    },
+    tick
+    }}
+    """
+    request1 = do_request(query1)
+    for pool in request1['data']['pools']:
+        uni3pools += [pool]
+        #print(pool.keys())
+    #print(uni3pools)
+    goOn = False
+    while goOn:
+        lastTime = uni3pools[-1]['createdAtTimestamp']
+        print(lastTime)
+        query2_1 = "{pools(first:1000,orderBy:createdAtTimestamp,orderDirection:asc where:{createdAtTimestamp_gt:"
+        query2_3 = "}){id,token0{id,symbol,decimals},token1{id,symbol,decimals},feeTier,createdAtBlockNumber,createdAtTimestamp,ticks(where:{liquidityNet_gt: 0}, orderBy:tickIdx, orderDirection:asc){id,tickIdx,liquidityNet,liquidityGross,price0,price1},tick}}"
+        query = query2_1 + lastTime + query2_3
+        requestN = do_request(query)
+        if requestN['data']['pools'] == []:
+            #print(requestN)
+            goOn = False
+            break
+        for pool in requestN['data']['pools']:
+            uni3pools += [pool]
+    print(len(uni3pools))
+    return uni3pools
+
+
+
+    
+    
 
 def getPools():
     query = """
 
 {
     pool(id: "0x04916039b1f59d9745bf6e0a21f191d1e0a84287") {
-    id
-    token0{id symbol}
-    token1{id symbol}
-    feeTier
-    liquidity
+    id,
+    token0{id, symbol},
+    token1{id, symbol},
+    feeTier,
+    liquidity,
     ticks(where: {liquidityNet_gt: 0}, orderBy: tickIdx, orderDirection: asc) {
-        tickIdx
+        tickIdx,
         liquidityNet
     }
     
@@ -58,13 +133,6 @@ def getPools_Ticks(addrs):
         _ticks += [TICKS]
     return (_addresses, _ticks)
 
-
-class Uni3Pool:
-    def __init__(self, pool_data):
-        self.address = pool_data[0]
-        self.price_current = pool_data[1]
-        self.tick_current = pool_data[2]
-    
 
 def getAmount0Delta(priceA, priceB, liquidity, roundUp):
     if priceA > priceB:
@@ -231,6 +299,7 @@ def computeSwapStep(price_current, price_target, liquidity, amount_remaining, fe
 
 
 def main():
+#    priority_fee("2 gwei")
     # pool = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640"
     #yfi_eth =  "0x04916039b1f59d9745bf6e0a21f191d1e0a84287"
     #t = "0x2e5848efcfac935dd243c9094048ac346e198e1d"
@@ -238,11 +307,11 @@ def main():
     #ticks = [-138180,-23040,0,6960,7320,11280,12060,12780,14580,16080,16680,17700,17940,18780,19200,20040,20940,21180,21960,24000,24840,25080,25200,25800,26100,26340,26400,26580,26880,27060,27240,27360,27420,27540,28140,28320,28680,29700]
 #    print(u.getTickData(yfi_eth, ticks))
     #u = Uni3ticks.deploy({"from": accounts[0]})
-    u = Uni3ticks[-1]
-    data = getPools()
-    pools = data['data']['pool']
+    #u = Uni3ticks[-1]
+    #data = getPools()
+    #pools = data['data']['pool']
  #   print(pools)
-    
+    getAllPools()
     # #    ticks = pools[0]['ticks']
     # addrs = []
 #     # ticks = [[]]
@@ -255,9 +324,9 @@ def main():
 # #    print(ticks)
 # #    print(u.getTickData(addresses[0], ticks[0]))
 #     print(addresses, ticks)
-    (address, ticks) = ticksAndAddr(pools)
-    c = u.forPools([address], [ticks])
-    print(c[0])
+    #(address, ticks) = ticksAndAddr(pools)
+    #c = u.forPools([address], [ticks])
+    #print(c[0])
 #    tick_array = []
 #    for i in c:
 #        print(i)
@@ -274,24 +343,24 @@ def main():
     
     #print(c[0][1])
     # print(tick_array)
-    currentPrice = c[0][1]
-    nextPrice = 349763628036266027723339106301
-#    nextPrice = 79156945126914824732836954
-    fee = 3000
-    liquidity = c[0][3]
-    amountRemaining = 10**18
-    print(currentPrice, nextPrice, fee, liquidity, amountRemaining)
-    print(computeSwapStep(currentPrice, nextPrice, liquidity, amountRemaining, fee)) #(price_next, amountIn, amountOut, feeAmount)
+  #   currentPrice = c[0][1]
+#     nextPrice = 349763628036266027723339106301
+# #    nextPrice = 79156945126914824732836954
+#     fee = 3000
+#     liquidity = c[0][3]
+#     amountRemaining = 10**18
+#     print(currentPrice, nextPrice, fee, liquidity, amountRemaining)
+#     print(computeSwapStep(currentPrice, nextPrice, liquidity, amountRemaining, fee)) #(price_next, amountIn, amountOut, feeAmount)
 
 
-    currentPrice = c[0][1]
-#    nextPrice = 349763628036266027723339106301
-    nextPrice = 79156945126914824732836954
-    fee = 3000
-    liquidity = c[0][3]
-    amountRemaining = 10**18
-    print(currentPrice, nextPrice, fee, liquidity, amountRemaining)
-    print(computeSwapStep(currentPrice, nextPrice, liquidity, amountRemaining, fee)) #(price_next, amountIn, amountOut, feeAmount)
+#     currentPrice = c[0][1]
+# #    nextPrice = 349763628036266027723339106301
+#     nextPrice = 79156945126914824732836954
+#     fee = 3000
+#     liquidity = c[0][3]
+#     amountRemaining = 10**18
+#     print(currentPrice, nextPrice, fee, liquidity, amountRemaining)
+#     print(computeSwapStep(currentPrice, nextPrice, liquidity, amountRemaining, fee)) #(price_next, amountIn, amountOut, feeAmount)
 
     #    print(addrs)
  #   da = getPools_Ticks(addrs)
